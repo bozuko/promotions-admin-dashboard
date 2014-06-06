7f7c255c1867c20621aab5d048902fd8d4491a00
Mark Fabrizio <fabrizim@owlwatch.com>
2014-06-06 09:40:57 -0400

    Different charts for different series data

--- assets/javascripts/promotions-charts.js
+++ assets/javascripts/promotions-charts.js
@@ -136,7 +136,7 @@ jQuery(function($){
         dataSource: this.data,
         commonSeriesSettings : {
           argumentField : 'start',
-          type: 'stackedBar'
+          type: series[0].length > 6 ? 'stackedArea' : "stackedBar"
         },
         series : series,
         legend: {
