6a754e090016451d318d8decb69c3e937f210b52
Mark Fabrizio <fabrizim@owlwatch.com>
2014-06-06 09:28:51 -0400

    Add event listener for promotion chooser

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -1,5 +1,9 @@
 jQuery(function($){
   var $container = $('#dashboard-widgets')
     , $welcome = $container.find('#dashboard_promotions_welcome')
-  
+    , $chooser = $('#promotion-chooser')
+    
+  $chooser.on('change', function(){
+    window.location = 'index.php?promotion_id='+$(this).find('option:selected').val();
+  });
 });
